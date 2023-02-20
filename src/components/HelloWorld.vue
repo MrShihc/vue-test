d75ec4a50fa8e62b8f621fff19bb87c067474b0b
shihaichao <shc0108@163.com>
2023-02-20 11:42:23 +0800

    11

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
+      
       </li>
       <li>
         <a
